bebbe3e05fb6f92bf7c599e70416eee95c33ebe3
vinayak5118 <143994802+vinayak5118@users.noreply.github.com>
2023-09-04 10:30:30 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,29 @@
+// to run this file ->  node 01_basics/data_types.js
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) we are using nodejs , not abrowser
+
+console.log( 3 
+    + 
+    3) //code readability should be high
+
+console.log("hitesh")
+
+//data types
+let name="hitesh" //string
+let age= 36         //integer
+let isLoggedin=true     //boolean
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null=>stand alone value
+//undefined=>value not assigned
+//symbol=>unique
+
+//object
+
+console.log(typeof "vinayak") //string 
+console.log(typeof null) //object //interview question
+console.log(typeof undefined) //undefined 
